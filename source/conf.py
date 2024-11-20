9c912eb805233e9a2b4a1c39f036dffeb3240bcc
Marc-Antoine Drouin <marc-antoine.drouin@lmd.ipsl.fr>
2024-11-20 09:04:04 +0100

    fix: logo in footer again

--- source/conf.py
+++ source/conf.py
@@ -13,6 +13,7 @@ author = "ACTRIS-CCRES"
 release = "1.0"
 
 BASE_URL = "https://github.com/ACTRIS-CCRES/docs"
+DEPLOY_URL = "https://ccres.ipsl.fr/docs"
 LOGO_PATH = Path(__file__).parent / "assets" / "logo_actris_ccress.png"
 
 # -- General configuration ---------------------------------------------------
@@ -101,25 +102,25 @@ html_theme_options = {
     "use_sidenotes": True,
     "home_page_in_toc": False,
     "show_toc_level": 2,
-    "extra_footer": """
+    "extra_footer": f"""
     <div style="display: flex; justify-content: space-between;" >
         <a href="https://www.cnrs.fr">
-            <img src="_static/institutions/logo_cnrs.png" alt="CNRS logo" height="50px">
+            <img src="{DEPLOY_URL}/_static/institutions/logo_cnrs.png" alt="CNRS logo" height="50px">
         </a>
         <a href="https://www.uvsq.fr/">
-            <img src="_static/institutions/logo_uvsq.png" alt="UVSQ logo" height="50px">
+            <img src="{DEPLOY_URL}/_static/institutions/logo_uvsq.png" alt="UVSQ logo" height="50px">
         </a>
         <a href="https://www.polytechnique.fr/">
-            <img src="_static/institutions/logo_polytechnique.png" alt="polytechnique logo" height="50px">
+            <img src="{DEPLOY_URL}/_static/institutions/logo_polytechnique.png" alt="polytechnique logo" height="50px">
         </a>
         <a href="https://www.fmi.fi/">
-            <img src="_static/institutions/logo_fmi.png" alt="FMI logo" height="50px">
+            <img src="{DEPLOY_URL}/_static/institutions/logo_fmi.png" alt="FMI logo" height="50px">
         </a>
         <a href="https://www.tudelft.nl/en/">
-            <img src="_static/institutions/logo_tu-delft.png" alt="TU-delft logo" height="50px">
+            <img src="{DEPLOY_URL}/_static/institutions/logo_tu-delft.png" alt="TU-delft logo" height="50px">
         </a>
         <a href="https://www.uni-koeln.de/">
-            <img src="_static/institutions/logo_uni-koln.jpg" alt="uni koln logo" height="50px">
+            <img src="{DEPLOY_URL}/_static/institutions/logo_uni-koln.jpg" alt="uni koln logo" height="50px">
         </a>
     </div>
     """,
